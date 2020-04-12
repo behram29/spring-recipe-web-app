13d263abca21202ba2165fa3a4022e4f5fec96df
Bahram Sattarli <mr.settarli97@gmail.com>
2020-04-12 19:14:09 +0400

    Initial Commit

---
+++ src/main/java/bahram/springframework/springrecipewebapp/SpringRecipeWebAppApplication.java
@@ -0,0 +1,13 @@
+package bahram.springframework.springrecipewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipeWebAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/bahram/springframework/springrecipewebapp/SpringRecipeWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package bahram.springframework.springrecipewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeWebAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

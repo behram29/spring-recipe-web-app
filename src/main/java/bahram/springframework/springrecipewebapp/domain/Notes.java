2749ddac86d1e5e10fa3d192aaed35b3a4704839
Bahram Sattarli <mr.settarli97@gmail.com>
2020-04-25 19:40:23 +0400

    Resolving Bug

--- src/main/java/bahram/springframework/springrecipewebapp/domain/Notes.java
+++ src/main/java/bahram/springframework/springrecipewebapp/domain/Notes.java
@@ -8,8 +8,10 @@ public class Notes {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @OneToOne
     private Recipe recipe;
+
     @Lob
     private String recipeNotes;
 
--- src/main/java/bahram/springframework/springrecipewebapp/domain/Recipe.java
+++ src/main/java/bahram/springframework/springrecipewebapp/domain/Recipe.java
@@ -11,12 +11,15 @@ public class Recipe {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String description;
     private Integer prepTime;
     private Integer cookTime;
     private Integer servings;
     private String source;
     private String url;
+
+    @Lob
     private String directions;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
@@ -33,7 +36,7 @@ public class Recipe {
 
     @ManyToMany
     @JoinTable(name = "recipe_category",
-        joinColumns = @JoinColumn(name = "recipe_id"),
+            joinColumns = @JoinColumn(name = "recipe_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
@@ -115,6 +118,13 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+        notes.setRecipe(this);
+    }
+
+    public Recipe addIngredient(Ingredient ingredient){
+        ingredient.setRecipe(this);
+        this.ingredients.add(ingredient);
+        return this;
     }
 
     public Set<Ingredient> getIngredients() {

403f52760591a7257e66765dff23209ff936e987
Bahram Sattarli <mr.settarli97@gmail.com>
2020-04-28 22:29:18 +0400

    Added jUnit Test to Category domain class

--- src/test/java/bahram/springframework/springrecipewebapp/SpringRecipeWebAppApplicationTests.java
+++ src/test/java/bahram/springframework/springrecipewebapp/SpringRecipeWebAppApplicationTests.java
@@ -1,9 +1,5 @@
 package bahram.springframework.springrecipewebapp;
 
-//import org.junit.jupiter.api.Test;
-
-
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
